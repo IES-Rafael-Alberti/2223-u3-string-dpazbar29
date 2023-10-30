6922eb17dc539ebe5d3df24f023ba4c367d6cff6
dpazbar29 <145806563+dpazbar29@users.noreply.github.com>
2023-10-30 12:39:44 +0100

    Delete venv directory

--- venv/src/E_3_0_2.py
+++
@@ -1,8 +0,0 @@
-#3.0.2
-"Dado que fruta es una variable de tipo cadena, ¿qué significa fruta[:]?"
-
-'''
-Esta función es la función slice,si no se introducen parametros simplemente
-seguirá devolviendo la palabra fruta
-
-'''
